ad30053a1b23bc8e1322a5d8c39dc23195f36178
Adam Bertram <adbertram@gmail.com>
2019-04-17 12:07:40 -0500

    Error handling in GetConfiguration

--- Private/GetConfiguration.ps1
+++ Private/GetConfiguration.ps1
@@ -2,13 +2,27 @@ function GetConfiguration {
 	[OutputType('pscustomobject')]
 	[CmdletBinding()]
 	param
-	(
-		[Parameter()]
-		[ValidateNotNullOrEmpty()]
-		[string]$Region = $config:tokenRegion
-	)
+	()
 
 	$ErrorActionPreference = 'Stop'
 	
-	Get-Content -Path "$($PSScriptRoot | Split-Path -Parent)\configuration.json" | ConvertFrom-Json
+	$configFilePath = "$($PSScriptRoot | Split-Path -Parent)\configuration.json"
+	if (-not (Test-Path -Path $configFilePath -PathType Leaf)) {
+		throw "Required configuration file [$($configFilePath)] was not found."
+	}
+	$config = Get-Content -Path $configFilePath | ConvertFrom-Json
+
+	$requiredAttributes = 'TokenEndpoint', 'SubscriptionRegion'
+	$reqCogServAttributes = 'Name', 'ResourceGroupName'
+	foreach ($attrib in $requiredAttributes) {
+		if (-not $config.$attrib) {
+			throw "[$($attrib)] in $configFilePath is not set."
+		}
+	}
+	foreach ($attrib in $reqCogServAttributes) {
+		if (-not $config.CognitiveServicesAccount.$attrib) {
+			throw "CognitiveServicesAccount [$($attrib)] in $configFilePath is not set."
+		}
+	}
+	$config
 }
